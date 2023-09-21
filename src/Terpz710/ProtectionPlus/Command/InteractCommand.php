4b1e822bc721988dc3096b97e902d2de7a6d8bb5
BaysXotic <145719670+BaysXotic@users.noreply.github.com>
2023-09-21 16:03:49 -0700

    Update InteractCommand.php

--- src/Terpz710/ProtectionPlus/Command/InteractCommand.php
+++ src/Terpz710/ProtectionPlus/Command/InteractCommand.php
@@ -4,52 +4,39 @@ declare(strict_types=1);
 
 namespace Terpz710\ProtectionPlus\Command;
 
-use pocketmine\block\Block;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\event\block\BlockInteractEvent;
+use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\Listener;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 
-class InteractCommand extends Command implements Listener {
-
-    private $interactionActive = [];
+class InteractionCommand extends Command implements Listener {
 
     public function __construct(PluginBase $plugin) {
-        parent::__construct("interact", "Toggle block interaction");
-        $this->setPermission("protectionplus.interact");
+        parent::__construct("interaction", "Toggle block interaction");
         $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
     }
 
     public function execute(CommandSender $sender, string $label, array $args): bool {
-        // Your execute method code here
+        if ($sender instanceof Player) {
+            $sender->sendMessage("Interaction blocking is active.");
+        } else {
+            $sender->sendMessage("This command can only be used in-game");
+        }
+        return true;
     }
 
     /**
-     * @param BlockInteractEvent $event
+     * @param PlayerInteractEvent $event
      * @priority HIGHEST
      */
-    public function onInteract(BlockInteractEvent $event): void {
+    public function onPlayerInteract(PlayerInteractEvent $event): void {
         $player = $event->getPlayer();
-        $world = $player->getWorld()->getFolderName();
-        $block = $event->getBlock();
-
-        if (isset($this->interactionActive[$world])) {
-            // Define a list of block types that should be allowed
-            $allowedBlockTypes = [
-                Block::CRAFTING_TABLE,  // Allow interaction with crafting tables
-                Block::CHEST,           // Allow interaction with chests
-                Block::BREWING_STAND,   // Allow interaction with brewing stands
-                // Add more block types as needed
-            ];
-
-            // Check if the block's block type is in the list of allowed block types
-            if (!in_array($block->getId(), $allowedBlockTypes)) {
-                // Block type is not allowed, cancel interaction
-                $player->sendMessage("Block interaction is active in this world. You cannot interact with this block.");
-                $event->setCancelled();
-            }
+        
+        if ($player->getInventory()->getItemInHand()->getId() !== 0) {
+            $player->sendMessage("Interaction blocking is active. You cannot use items.");
+            $event->cancel();
         }
     }
 }
