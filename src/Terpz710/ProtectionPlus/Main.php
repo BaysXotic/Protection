567904a22b0753df69dd95d4c8806f16fe414371
BaysXotic <145719670+BaysXotic@users.noreply.github.com>
2023-09-21 15:55:12 -0700

    Update Main.php

--- src/Terpz710/ProtectionPlus/Main.php
+++ src/Terpz710/ProtectionPlus/Main.php
@@ -6,10 +6,12 @@ namespace Terpz710\ProtectionPlus;
 
 use pocketmine\plugin\PluginBase;
 use Terpz710\ProtectionPlus\Command\ProtectCommand;
+use Terpz710\ProtectionPlus\Command\InteractCommand; // Add this line to import the InteractCommand class
 
 class Main extends PluginBase {
 
     public function onEnable(): void {
         $this->getServer()->getCommandMap()->register("protection", new ProtectCommand($this));
+        $this->getServer()->getCommandMap()->register("interact", new InteractCommand($this)); // Register the InteractCommand
     }
 }
