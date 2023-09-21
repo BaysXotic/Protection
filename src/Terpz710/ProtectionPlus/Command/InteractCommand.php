4ff50a90da6acc7179baf68b46c03466d18d7b75
BaysXotic <145719670+BaysXotic@users.noreply.github.com>
2023-09-21 16:17:30 -0700

    Update InteractCommand.php

--- src/Terpz710/ProtectionPlus/Command/InteractCommand.php
+++ src/Terpz710/ProtectionPlus/Command/InteractCommand.php
@@ -6,21 +6,44 @@ namespace Terpz710\ProtectionPlus\Command;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\inventory\InventoryOpenEvent;
 use pocketmine\event\Listener;
-use pocketmine\player\Player;
+use pocketmine\inventory\BaseInventory;
+use pocketmine\inventory\CraftingGrid;
+use pocketmine\inventory\CreativeInventory;
+use pocketmine\inventory\PlayerInventory;
 use pocketmine\plugin\PluginBase;
 
 class InteractCommand extends Command implements Listener {
 
     public function __construct(PluginBase $plugin) {
         parent::__construct("interaction", "Toggle block interaction");
+        $this->setPermission("protectionplus.interaction");
         $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
     }
 
     public function execute(CommandSender $sender, string $label, array $args): bool {
         if ($sender instanceof Player) {
-            $sender->sendMessage("Interaction blocking is active.");
+            if (!$this->testPermission($sender)) {
+                $sender->sendMessage("You do not have permission to use this command");
+                return true;
+            }
+
+            $world = $sender->getWorld()->getFolderName();
+            $action = strtolower($args[0] ?? "");
+
+            switch ($action) {
+                case "on":
+                    $this->protectionActive[$world] = true;
+                    $sender->sendMessage("Block interaction blocking is now active in the $world.");
+                    break;
+                case "off":
+                    unset($this->protectionActive[$world]);
+                    $sender->sendMessage("Block interaction blocking is now inactive in the $world.");
+                    break;
+                default:
+                    $sender->sendMessage("Usage: /interaction <on|off>");
+            }
         } else {
             $sender->sendMessage("This command can only be used in-game");
         }
@@ -28,14 +51,15 @@ class InteractCommand extends Command implements Listener {
     }
 
     /**
-     * @param PlayerInteractEvent $event
+     * @param InventoryOpenEvent $event
      * @priority HIGHEST
      */
-    public function onPlayerInteract(PlayerInteractEvent $event): void {
+    public function onInventoryOpen(InventoryOpenEvent $event): void {
         $player = $event->getPlayer();
-        
-        if ($player->getInventory()->getItemInHand()->getItem() !== 0) {
-            $player->sendMessage("Interaction blocking is active. You cannot use items.");
+        $inventory = $event->getInventory();
+
+        if ($inventory instanceof CraftingGrid || $inventory instanceof BaseInventory) {
+            $player->sendMessage("Inventory interaction is blocked.");
             $event->cancel();
         }
     }
