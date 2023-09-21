5fc222d131dead94c3788511cd5aec3c90ec3abc
Spooffyyy <145518793+Spooffyyy@users.noreply.github.com>
2023-09-20 22:10:43 -0700

    Update ProtectCommand.php

--- src/Terpz710/ProtectionPlus/Command/ProtectCommand.php
+++ src/Terpz710/ProtectionPlus/Command/ProtectCommand.php
@@ -6,16 +6,21 @@ namespace Terpz710\ProtectionPlus\Command;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
 use pocketmine\player\Player;
 use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginOwnedTrait;
 
-class PvPCommand extends Command {
+class ProtectionCommand extends Command implements Listener {
     use PluginOwnedTrait;
 
     public function __construct(Plugin $owningPlugin) {
-        parent::__construct("pvp", "Enable or disable PvP protection", null, [$owningPlugin]);
-        $this->setPermission("protectionplus.pvp");
+        parent::__construct("protection", "Enable or disable protection", null, [$owningPlugin]);
+        $this->setPermission("protectionplus.protection");
+        $owningPlugin->getServer()->getPluginManager()->registerEvents($this, $owningPlugin);
     }
 
     public function execute(CommandSender $sender, string $label, array $args): bool {
@@ -26,7 +31,7 @@ class PvPCommand extends Command {
             }
 
             if (empty($args)) {
-                $sender->sendMessage("Usage: /pvp <on|off>");
+                $sender->sendMessage("Usage: /protection <on|off>");
                 return false;
             }
 
@@ -34,13 +39,13 @@ class PvPCommand extends Command {
 
             switch ($subcommand) {
                 case "on":
-                    $this->enablePvP($sender);
+                    $this->enableProtection($sender);
                     break;
                 case "off":
-                    $this->disablePvP($sender);
+                    $this->disableProtection($sender);
                     break;
                 default:
-                    $sender->sendMessage("Usage: /pvp <on|off>");
+                    $sender->sendMessage("Usage: /protection <on|off>");
             }
         } else {
             $sender->sendMessage("This command can only be used in-game.");
@@ -48,15 +53,48 @@ class PvPCommand extends Command {
         return true;
     }
 
-    private function enablePvP(Player $player): void {
-        $player->setPvP(true);
-        $player->sendMessage("PvP protection is now disabled!");
-        $player->sendTitle("PvP Enabled", "", 10, 40, 10);
+    private function enableProtection(Player $player): void {
+        $player->sendMessage("Protection is now enabled!");
+        $player->sendTitle("Protection Enabled", "", 10, 40, 10);
+        $this->getOwningPlugin()->setProtectionEnabled($player, true);
     }
 
-    private function disablePvP(Player $player): void {
-        $player->setPvP(false);
-        $player->sendMessage("PvP protection is now enabled!");
-        $player->sendTitle("PvP Disabled", "", 10, 40, 10);
+    private function disableProtection(Player $player): void {
+        $player->sendMessage("Protection is now disabled!");
+        $player->sendTitle("Protection Disabled", "", 10, 40, 10);
+        $this->getOwningPlugin()->setProtectionEnabled($player, false);
+    }
+
+    /**
+     * @param BlockBreakEvent $event
+     * @priority MONITOR
+     */
+    public function onBlockBreak(BlockBreakEvent $event): void {
+        $player = $event->getPlayer();
+        if (!$this->getOwningPlugin()->isProtectionEnabled($player)) {
+            $event->setCancelled(true);
+        }
+    }
+
+    /**
+     * @param BlockPlaceEvent $event
+     * @priority MONITOR
+     */
+    public function onBlockPlace(BlockPlaceEvent $event): void {
+        $player = $event->getPlayer();
+        if (!$this->getOwningPlugin()->isProtectionEnabled($player)) {
+            $event->setCancelled(true);
+        }
+    }
+
+    /**
+     * @param PlayerDropItemEvent $event
+     * @priority MONITOR
+     */
+    public function onPlayerDropItem(PlayerDropItemEvent $event): void {
+        $player = $event->getPlayer();
+        if (!$this->getOwningPlugin()->isProtectionEnabled($player)) {
+            $event->setCancelled(true);
+        }
     }
 }
