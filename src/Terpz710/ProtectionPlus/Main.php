27faa1e370daa279c53da1791cdfcd94ceab1722
Spooffyyy <145518793+Spooffyyy@users.noreply.github.com>
2023-09-21 09:19:48 -0700

    Update Main.php

--- src/Terpz710/ProtectionPlus/Main.php
+++ src/Terpz710/ProtectionPlus/Main.php
@@ -5,14 +5,12 @@ declare(strict_types=1);
 namespace Terpz710\ProtectionPlus;
 
 use pocketmine\plugin\PluginBase;
-use Terpz710\ProtectionPlus\Command\InteractCommand;
 use Terpz710\ProtectionPlus\Command\PvPCommand;
 use Terpz710\ProtectionPlus\Command\ProtectionCommand;
 
 class Main extends PluginBase {
 
-    public function onEnable(): void {
-        $this->getServer()->getCommandMap()->register("interact", new InteractCommand($this));
+    public function onEnable(): void 
         $this->getServer()->getCommandMap()->register("pvp", new PvPCommand($this));
         $this->getServer()->getCommandMap()->register("protection", new ProtectionCommand($this));
     }
